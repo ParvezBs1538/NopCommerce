0f6e15b8e95be2f740738338c8ff46e8669d1761
Masud Parvez <masud.parvez@brainstation.com>
2024-07-30 06:55:19 +0600

    update

---
+++ Plugins/Nop.Plugin.Widgets.Practice/InventoryPlugin.cs
@@ -0,0 +1,48 @@
+﻿using Nop.Core;
+using Nop.Services.Cms;
+using Nop.Services.Localization;
+using Nop.Services.Plugins;
+using Nop.Web.Framework.Infrastructure;
+
+namespace Nop.Plugin.Widgets.Practice
+{
+    public class InventoryPlugin : BasePlugin, IWidgetPlugin
+    {
+        private readonly IWebHelper _webHelper;
+        private readonly ILocalizationService _localizationService;
+
+        public bool HideInWidgetList => false;
+
+        public InventoryPlugin(IWebHelper webHelper,
+            ILocalizationService localizationService)
+        {
+            _webHelper = webHelper;
+            _localizationService = localizationService;
+        }
+
+        public override string GetConfigurationPageUrl()
+        {
+            return $"{_webHelper.GetStoreLocation()}Areas/Admin/Trainer/List";
+        }
+
+        public override async Task InstallAsync()
+        {
+            await base.InstallAsync();
+        }
+
+        public override async Task UninstallAsync()
+        {
+            await base.UninstallAsync();
+        }
+
+        public Task<IList<string>> GetWidgetZonesAsync()
+        {
+            return Task.FromResult<IList<string>>([PublicWidgetZones.HomepageBeforeProducts, PublicWidgetZones.HomepageBottom]);
+        }
+
+        public Type GetWidgetViewComponent(string widgetZone)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
